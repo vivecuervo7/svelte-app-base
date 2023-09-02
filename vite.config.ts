ccdc04df9d602b322b1debcca62d5a91c9531295
Isaac Dedini <isaac.dedini@purple.telstra.com>
2023-09-02 15:56:17 +1000

    Apply prettier to codebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({

b20c1f9eb1249e7bfaa873465111659bdefe63c4
Isaac Dedini <isaac.dedini@purple.telstra.com>
2023-09-03 12:19:08 +1000

    Add svelte-routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,14 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
-  import Logos from './lib/Logos.svelte'
+  import { Router, Route, links } from 'svelte-routing'
+  import Landing from 'src/lib/Landing.svelte'
 </script>
 
-<main>
-  <Logos />
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+<main use:links>
+  <Router>
+    <Route path="/" component={Landing} />
+    <Route>
+      <p>Not found</p>
+      <a href="/">Go back</a>
+    </Route>
+  </Router>
 </main>
-
-<style lang="postcss">
-  h1 {
-    @apply text-[3.2em] leading-[1.1em];
-  }
-
-  .card {
-    @apply p-[2.5em];
-  }
-
-  .read-the-docs {
-    @apply text-[#888] leading-9;
-  }
-</style>
---
+++ src/lib/Landing.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import Counter from 'src/lib/Counter.svelte'
+  import Logos from 'src/lib/Logos.svelte'
+</script>
+
+<div>
+  <Logos />
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+
+  <a href="/not-found">Go to page that doesn't exist</a>
+</div>
+
+<style lang="postcss">
+  h1 {
+    @apply text-[3.2em] leading-[1.1em];
+  }
+
+  .card {
+    @apply p-[2.5em];
+  }
+
+  .read-the-docs {
+    @apply text-[#888] leading-9;
+  }
+</style>
